5938340cebe7fcf612c849708f3454e4e28cdbc6
Phuc Bui <phuchoangb228@gmail.com>
2026-06-02 09:36:17 +0700

    test source pakage

---
+++ Gr3_ProBuildPC/src/java/emtity/User.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package emtity;
+
+/**
+ *
+ * @author admin
+ */
+public class User {
+    String name;
+    int ID;
+
+    public User(String name, int ID) {
+        this.name = name;
+        this.ID = ID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+    
+}

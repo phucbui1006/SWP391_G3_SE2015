786fd0c6425a24853dbc7b9b3a9da9a12bd48ea4
Phuc Bui <phuchoangb228@gmail.com>
2026-06-24 14:50:38 +0700

    gộp file spl

--- Gr3_ProBuildPC/database/fix-category-id.sql
+++
@@ -1,5 +0,0 @@
--- Fix the category with ID 19 (test) to be ID 11
-UPDATE categories SET category_id = 11 WHERE category_id = 19;
-
--- Reset the AUTO_INCREMENT of categories table to 12 so that the next category will have ID 12
-ALTER TABLE categories AUTO_INCREMENT = 12;
